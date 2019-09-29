c435f5749ed0d3584d6352cd11178e781a8118db
To-Fujita <53250813+To-Fujita@users.noreply.github.com>
2019-09-30 08:49:27 +0900

    Add files via upload

--- is/sample_phaser_01.js
+++ is/sample_phaser_01.js
@@ -1,4 +1,4 @@
-// Sample for phaser.js by T. Fujita on 2019/9/11 (Display a static character)
+﻿// Sample for phaser.js by T. Fujita on 2019/9/11 (Display a static character)
 
 var config = {
     type: Phaser.AUTO,
